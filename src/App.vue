1c82b77e7ed766aad40a1b08e8beae2d3ff74baf
Рогов Дмитрий <d.rogov@autorus.ru>
2023-06-21 17:16:09 +0300

    Extract form component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { computed, reactive, ref } from "vue";
+import MovieForm from "@/MovieForm.vue";
 import MovieItem from "@/MovieItem.vue";
 import { items } from "./movies.json";
+import { computed, ref } from "vue";
 
 const movies = ref(items);
+const currentMovie = ref();
 
 function updateRating(id, rating) {
   movies.value = movies.value.map((movie) => {
@@ -13,145 +15,42 @@ function updateRating(id, rating) {
     return movie;
   });
 }
-
 function removeMovie(id) {
   movies.value = movies.value.filter((movie) => movie.id !== id);
 }
-
 function editMovie(id) {
-  const movie = movies.value.find((movie) => movie.id === id);
-
-  form.id = movie.id;
-  form.name = movie.name;
-  form.description = movie.description;
-  form.image = movie.image;
-  form.inTheaters = movie.inTheaters;
-  form.genres = movie.genres;
-
+  currentMovie.value = movies.value.find((movie) => movie.id === id);
   showForm();
 }
-
-const errors = reactive({
-  name: null,
-  description: null,
-  image: null,
-  inTheaters: null,
-  genres: null,
-});
-const form = reactive({
-  id: null,
-  name: null,
-  description: null,
-  image: null,
-  inTheaters: false,
-  genres: null,
-});
-const validations = reactive({
-  name: "required",
-  genres: "required",
-});
-
-const genres = reactive([
-  { text: "Drama", value: "Drama" },
-  { text: "Crime", value: "Crime" },
-  { text: "Action", value: "Action" },
-  { text: "Comedy", value: "Comedy" },
-]);
-
-const validationRules = (rule) => {
-  if (rule === "required") return /^ *$/;
-
-  return null;
-};
-
-function validate() {
-  let valid = true;
-  clearErrors();
-  for (const [field, rule] of Object.entries(validations)) {
-    const validation = validationRules(rule);
-    if (validation) {
-      if (validation.test(form[field] || "")) {
-        errors[field] = `${field} is ${rule}`;
-        valid = false;
-      }
-    }
-  }
-
-  return valid;
-}
-
-function saveMovie() {
-  if (form.id) {
-    updateMovie();
+function saveMovie(data) {
+  const isNew = !!movies.value.find((movie) => movie.id === data.id);
+  if (!isNew) {
+    addMovie(data);
   } else {
-    addMovie();
+    updateMovie(data);
   }
 }
-
-function updateMovie() {
-  if (validate()) {
-    const movie = {
-      id: form.id,
-      name: form.name,
-      description: form.description,
-      image: form.image,
-      genres: form.genres,
-      inTheaters: form.inTheaters,
-      rating: null,
-    };
-
-    movies.value = movies.value.map((m) => {
-      if (m.id === movie.id) {
-        movie.rating = m.rating;
-        return movie;
-      }
-      return m;
-    });
-
-    hideForm();
-  }
+function updateMovie(data) {
+  movies.value = movies.value.map((m) => {
+    if (m.id === data.id) {
+      data.rating = m.rating;
+      return data;
+    }
+    return m;
+  });
+  hideForm();
 }
-
-function addMovie() {
-  if (validate()) {
-    const movie = {
-      id: Number(Date.now()),
-      name: form.name,
-      description: form.description,
-      image: form.image,
-      genres: form.genres,
-      inTheaters: form.inTheaters,
-      rating: null,
-    };
-    movies.value.push(movie);
-    hideForm();
-  }
-}
-
-function cleanUpForm() {
-  form.name = null;
-  form.description = null;
-  form.image = null;
-  form.genres = null;
-  form.inTheaters = false;
-  clearErrors();
-}
-
-function clearErrors() {
-  errors.name = null;
-  errors.description = null;
-  errors.image = null;
-  errors.genres = null;
-  errors.inTheaters = null;
+function addMovie(data) {
+  movies.value.push(data);
+  hideForm();
 }
 
 const showMovieForm = ref(false);
 
 function hideForm() {
   showMovieForm.value = false;
-  cleanUpForm();
+  currentMovie.value = null;
 }
-
 function showForm() {
   showMovieForm.value = true;
 }
@@ -160,14 +59,11 @@ const averageRating = computed(() => {
   const avg = movies.value
       .map((movie) => parseInt(movie.rating || 0))
       .reduce((a, b) => a + b, 0);
-
-  return Number(avg / movies.value.length).toFixed(1);
+  return avg ? Number(avg / movies.value.length).toFixed(1) : 0;
 });
-
 const totalMovies = computed(() => {
   return movies.value.length;
 });
-
 function removeRatings() {
   movies.value = movies.value.map((movie) => {
     movie.rating = null;
@@ -180,84 +76,11 @@ function removeRatings() {
   <div class="app">
     <div v-if="showMovieForm" class="modal-wrapper">
       <div class="modal-wrapper-inner">
-        <form @submit.prevent="saveMovie">
-          <input type="hidden" name="id" v-model="form.id" />
-          <div class="movie-form-input-wrapper">
-            <label for="name">Name</label>
-            <input
-                type="text"
-                name="name"
-                v-model="form.name"
-                class="movie-form-input"
-            />
-            <span class="movie-form-error">{{ errors.name }}</span>
-          </div>
-          <div class="movie-form-input-wrapper">
-            <label for="description">Description</label>
-            <textarea
-                type="text"
-                name="description"
-                v-model="form.description"
-                class="movie-form-textarea"
-            />
-            <span class="movie-form-error">{{ errors.description }}</span>
-          </div>
-          <div class="movie-form-input-wrapper">
-            <label for="image">Image</label>
-            <input
-                type="text"
-                name="image"
-                v-model="form.image"
-                class="movie-form-input"
-            />
-            <span class="movie-form-error">{{ errors.image }}</span>
-          </div>
-          <div class="movie-form-input-wrapper">
-            <label for="genre">Genres</label>
-            <select
-                name="genre"
-                v-model="form.genres"
-                class="movie-form-input"
-                multiple
-            >
-              <option
-                  v-for="option in genres"
-                  :key="option.value"
-                  :value="option.value"
-              >
-                {{ option.text }}
-              </option>
-            </select>
-            <span class="movie-form-error">
-              {{ errors.genres }}
-            </span>
-          </div>
-          <div class="movie-form-input-wrapper">
-            <label for="genre" class="movie-form-checkbox-label">
-              <input
-                  type="checkbox"
-                  v-model="form.inTheaters"
-                  :true-value="true"
-                  :false-value="false"
-                  class="movie-form-checkbox"
-              />
-              <span>In theaters</span>
-            </label>
-            <span class="movie-form-error">
-              {{ errors.inTheaters }}
-            </span>
-          </div>
-          <div class="movie-form-actions-wrapper">
-            <button type="button" class="button" @click="hideForm">
-              Cancel
-            </button>
-
-            <button type="submit" class="button-primary">
-              <span v-if="form.id">Update</span>
-              <span v-else>Create</span>
-            </button>
-          </div>
-        </form>
+        <MovieForm
+            @update:modelValue="saveMovie"
+            :modelValue="currentMovie"
+            @cancel="hideForm"
+        />
       </div>
     </div>
     <div class="movie-actions-list-wrapper">
